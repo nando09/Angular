afa530c368a91d740b7c93ef232b3874cbd9578b
nando09 <nando.online.fb@gmail.com>
2019-08-07 12:38:51 -0300

    Construindo um projeto d ingles

--- app1/src/app/topo/topo.component.ts
+++ app1/src/app/topo/topo.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core'
 
 @Component({
-	// Ira chamar o componente com o "app-topo"
-	selector: 'app-topo',
+	// // Ira chamar o componente com o "app-topo"
+	// selector: '[app-topo]', ********* Chama como um atributo da div
+	// selector: '.app-topo', ********* Chama como uma class da div
+	selector: 'app-topo', // ********* Chama normal somente com o nome
+
 	// Qual o html que a class vai controlar
-	templateUrl: './topo.component.html'
-	// 
+	templateUrl: './topo.component.html',
+	// Podendo fazer o HTML direto no template
 	// template: ''
+
+	// Podendo passar mais de um arquivo de css para o array
+	styleUrls: ['./topo.component.css']
+
+	// Aqui vai poder definir o estilo aqui mesmo, porem o de cima passa um arquivo para ler o css
+	// styles : [`.exemplo{ color:red }`]
 })
 export class TopoComponent {
 
