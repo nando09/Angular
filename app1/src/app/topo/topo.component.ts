109d3c22b75d3f64f3d4704b086dd7d4ab3fef0b
nando09 <nando.online@live.com>
2019-08-06 23:49:29 -0300

    Inico de classes em angular

--- app1/src/app/topo/topo.component.ts
+++ app1/src/app/topo/topo.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core'
 
 @Component({
-	// Ira chamar o componente com o "app-topo"
-	selector: 'app-topo',
-	// Qual o html que a class vai controlar
-	templateUrl: './topo.component.html'
-	// 
-	// template: ''
+    selector: 'app-topo',
+    templateUrl: './topo.component.html',
+    styleUrls: ['./topo.component.css']
 })
 export class TopoComponent {
-
+	public titulo = "Aprendendo Inglês";
 }
