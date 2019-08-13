7c8e12b2eab82df1768f7c2abec6d0c568fe5c0b
nando09 <nando.online.fb@gmail.com>
2019-08-13 17:33:15 -0300

    Template com angula

--- app2/src/app/home/home.component.ts
+++ app2/src/app/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model'
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css'],
+	providers: [ OfertasService ],
+
 })
 export class HomeComponent implements OnInit {
+	public ofertas: Oferta[]
 
-  constructor() { }
+	constructor(private ofertasService: OfertasService) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.ofertas = this.ofertasService.getOfertas()
+		console.log(this.ofertas)
+
+
+		// // Maneira errada de fazer uma injeção nesse arquivo
+		// let ofertas = new OfertasService()
+		// console.log(ofertas.getOfertas())
+	}
 
 }
